24338f0125369ef5b5f8f3204abed2429eebf2c7
usmannaveed-19 <usmannaveed302@gmail.com>
2023-10-15 01:15:56 +0500

    4th Commit

--- A1.py
+++ A1.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score,f1_score
+from sklearn.metrics import accuracy_score, f1_score
+
 
 def preprocessing():
     data = load_wine()
@@ -10,23 +11,26 @@ def preprocessing():
     df['target'] = data.target
     print("df: ", df)
     labels = df['target']
-    df.drop(columns=['target'], inplace=True)  
+    df.drop(columns=['target'], inplace=True)
     xtrain, xtest, ytrain, ytest = train_test_split(df, labels, test_size=0.3, random_state=42)
     ytrain = ytrain.values.ravel()
     ytest = ytest.values.ravel()
     return xtrain, xtest, ytrain, ytest
 
+
 xtrain, xtest, ytrain, ytest = preprocessing()
 
+
 def train_and_evaluate_random_forest(xtrain, ytrain, xtest, ytest, n_estimators=10, random_state=42):
-    
+
     RF = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
     RF.fit(xtrain, ytrain)
     ypred_rf = RF.predict(xtest)
     acc_rf = accuracy_score(ytest, ypred_rf) * 100
     f1_rf = f1_score(ytest, ypred_rf, average='macro') * 100
 
-    return acc_rf,f1_rf
+    return acc_rf, f1_rf
+
 
 # Example usage:
 acc_rf, f1_rf = train_and_evaluate_random_forest(xtrain, ytrain, xtest, ytest)
